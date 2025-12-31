7ed08c39e1caa63e7d1279194d6f24dd70af4201
schorchi75 <Jennifer.darmochwal@freenet.de>
2025-12-31 10:27:48 +0100

    impressum.php

--- impressum.php
+++ impressum.php
@@ -54,7 +54,7 @@
 
   <!-- Bootstrap & eigenes CSS -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-  <link rel="stylesheet" href="/css/style.css">
+  <link rel="stylesheet" href="/style.css">
 
   <!-- Bootstrap JS -->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" defer></script>
@@ -255,4 +255,5 @@
 
 </body>
 </html>
+
 
